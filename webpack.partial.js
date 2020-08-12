529523d29b8cc69b995cf1130cfb2737fa334636
HirenParekh <parekhhiren@outlook.com>
2020-08-12 15:17:46 +0530

    first commit

---
+++ webpack.partial.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          syntax: 'postcss-scss',
+          plugins: () => [
+            require('postcss-import'),
+            require('tailwindcss'),
+            require('autoprefixer'),
+          ],
+        },
+      },
+    ],
+  },
+}
